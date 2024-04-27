7c24529265a1da2accb107875b3a0c362e32a21f
ericjvanwinkle <eric.vanwinkle@gmail.com>
2024-04-27 01:47:42 -0400

    Update streamlit_app_parents.py

--- streamlit_app_parents.py
+++ streamlit_app_parents.py
@@ -18,7 +18,7 @@ name_on_order = st.text_input("Name on Smoothie")
 if name_on_order:
     st.write("The name on your Smoothie will be: ", name_on_order)
 
-cnx.connectiont("snowflake")
+cnx.connection("snowflake")
 session = cnx.get_session()
 my_dataframe = session.table("smoothies.public.fruit_options").select (col("FRUIT_NAME"))
 #st.dataframe(data=my_dataframe, use_container_width=True)
