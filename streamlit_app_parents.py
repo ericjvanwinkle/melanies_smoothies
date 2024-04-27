3ed79889947daf87d693b348d3a25e15ed59c664
ericjvanwinkle <eric.vanwinkle@gmail.com>
2024-04-27 01:36:26 -0400

    Update streamlit_app_parents.py

--- streamlit_app_parents.py
+++ streamlit_app_parents.py
@@ -1,10 +1,45 @@
 import streamlit as st
-#from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
-st.title(":green_salad: My Parents New Healthy Diner :cup_with_straw:")
-st.write("""Breakfast Menu""")
-st.write("Omega 3 & Blueberry Oatmeal")
-st.write("Kale, Spinach & Rocket Smoothie")
-st.write("Hard-Boiled Free Range Egg")
+# st.title(":green_salad: My Parents New Healthy Diner :cup_with_straw:")
+# st.write("""Breakfast Menu""")
+# st.write("Omega 3 & Blueberry Oatmeal")
+# st.write("Kale, Spinach & Rocket Smoothie")
+# st.write("Hard-Boiled Free Range Egg")
+
+# Write directly to the app
+st.title(":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
+st.write(
+    """Choose the fruit you want in your custom smoothie!"""
+)
+
+name_on_order = st.text_input("Name on Smoothie")
+if name_on_order:
+    st.write("The name on your Smoothie will be: ", name_on_order)
+
+cnx.connectiont("snowflake")
+session = cnx.get_session()
+my_dataframe = session.table("smoothies.public.fruit_options").select (col("FRUIT_NAME"))
+#st.dataframe(data=my_dataframe, use_container_width=True)
+ingredients_list = st.multiselect("Choose up to 5", my_dataframe)
+
+if ingredients_list:
+    st.write (ingredients_list)
+    st.text (ingredients_list)
+
+    ingredients_string = ''
+
+    for fruit_chosen in ingredients_list:
+        ingredients_string  += fruit_chosen + ' '
+
+    my_insert_stmt = """ insert into smoothies.public.orders(name_on_order, ingredients)
+            values ('""" + name_on_order + """', '""" + ingredients_string + """')"""
+
+    st.write(my_insert_stmt)
+
+    time_to_insert = st.button("Submit Order")
+    if time_to_insert:
+        session.sql(my_insert_stmt).collect()
+        st.success('Your Smoothie is ordered!', icon="✅")
+
